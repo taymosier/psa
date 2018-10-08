43cf53e100b04c12b3143d3dd27873b67f639738
Taylor <taymosier@gmail.com>
2018-10-08 15:45:35 -0400

    changed package.json / index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,44 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
-
-const express = require('express');
-const path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-const app = express();
-
-// Serve the static files from the React app
-app.use(logger('dev'));
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
-    res.json(list);
-    console.log('Sent list of items');
-});
-
-// Handles any requests that don't match the ones above
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
-app.listen(8080, "10.136.132.80");
-// app.listen(8080, "192.168.0.100");
-// app.listen(5000);
-
-console.log('App is listening');
-
-
 
 render((
     <BrowserRouter>
