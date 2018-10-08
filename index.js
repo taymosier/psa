a4825f243e9b27e990e30ebede97a699e7568a7f
Taylor <taymosier@gmail.com>
2018-10-07 23:50:51 -0400

    m

--- index.js
+++ index.js
@@ -7,15 +7,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
-try {
-  app.get('/api/getList', (req,res) => {
-      var list = ["item1", "item2", "item3"];
-      res.json(list);
-      console.log('Sent list of items');
-  });
-} catch(e) {
-  console.log(`outer try/catch\n ${e}`);
-}
+app.get('/api/getList', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
