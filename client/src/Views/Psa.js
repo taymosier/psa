d456dbc53296ee5a748dfcbc61189f38abd70da4
Taylor <taymosier@gmail.com>
2018-11-27 19:24:35 -0500

    m

--- client/src/Views/Psa.js
+++ client/src/Views/Psa.js
@@ -26,42 +26,9 @@ class Psa extends Component {
   render() {
     return (
     <div className="Psa">
-      {this.state.data !== ''
-       ?<div><PointSheet className={"pointsheet"} data={this.state.data} clearData={this.clearData} updateInfo={this.updateInfo} updateInventory={this.updateInventory}/></div>
-       :<div className={"homeOptionsView"}>
-          <h1 className={"homeHeader"}>GCC Point Sheets</h1>
-          <Button size="lg" className={"newPointSheetBtn"} onClick={this.getNewPointSheet}>
-            New Point Sheet
-          </Button>
-          <Button
-            className={"pointSheetListBtn"}
-            size="lg"
-            onClick={this.toggle}
-          >
-            Point Sheet List
-          </Button>
-          <PointSheetList
-            className={"pointSheetList"}
-            passSelectedDocument={this.passSelectedDocument}
-            isOpen={this.state.modal}
-            toggle={this.toggle}
-          />
-          <Button
-            className={"defaultEmailsListBtn"}
-            size="lg"
-            onClick={this.toggleDefaultEmailsModal}
-          >
-            Add/Remove Default Emails
-          </Button>
-          <DefaultEmailList
-            className={"defaultEmailsList"}
-            isOpen={this.state.defaultEmails}
-            toggle={this.toggleDefaultEmailsModal}
-          />
-        </div>
-      }
+      <div>This is the Point Sheet Application</div>
     </div>
     );
   }
 }
-export default Home;
+export default Psa;
